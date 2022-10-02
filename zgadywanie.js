369de995b828429b29561f545210adfafcd76c1e
Sysek123 <szymekdzik123@gmail.com>
2022-10-02 22:54:01 +0200

    morevideos

--- zgadywanie.js
+++ zgadywanie.js
@@ -2,8 +2,8 @@ const videoPlayer = document.querySelector('.video-player')
 const video = videoPlayer.querySelector('.video')
 const playButton = videoPlayer.querySelector('.play-button')
 // TODO asd
-var answers = ["Full Metal Alchemist Brotherhood","Fire Force","Dororo", "Parasyte"]
-var videoSources = ["FMAOP1.mp4","FF OP1.mp4", "Dororo OP1.mp4", "Parasyte OP1.mp4"]
+var answers = ["Full Metal Alchemist Brotherhood","Fire Force","Dororo", "Parasyte", "Baccano", "Blood C", "Blue Period", "Code geass", "Erased", "My hero academia"]
+var videoSources = ["files/FMAOP1.mp4","files/FF OP1.mp4", "files/Dororo OP1.mp4", "files/Parasyte OP1.mp4", "files/Baccano OP1.mp4", "files/Blood C OP1.mp4","files/Blue Period OP1.mp4", "files/Code geass OP1.mp4", "files/Erased OP1.mp4", "files/Hero OP1.mp4"]
 let choice = []
 i = 0
 
@@ -60,21 +60,43 @@ function removeElements() {
 
 function check() {
   choice.push(value)
+  var img = document.createElement("img")
+  img.src = "files/okejka.png"
+  var tag = document.getElementById("first-slot")
+  tag.appendChild(img)
   if (which == 0 && choice[i] == "Full Metal Alchemist Brotherhood" ) { 
     console.log('zgadłeś')
-    delete choice[0]
+    var img = document.createElement("img")
+    img.src = "files/okejka.png"
+    var tag = document.getElementById("first-slot")
+    tag.appendChild(img)
     i = i +1
   } else if (which == 1 && choice[i] == "Fire Force"){
     console.log('zgadłeś')
-    delete choice[0]; 
     i = i +1
   } else if (which == 2 && choice[i] == "Dororo"){
     console.log('zgadłeś')
-    delete choice[0]; 
     i = i +1
   } else if (which == 3 && choice[i] == "Parasyte"){
     console.log('zgadłeś')
-    delete choice[0]; 
+    i = i +1
+  } else if (which == 4 && choice[i] == "Baccano"){
+    console.log('zgadłeś')
+    i = i +1
+  } else if (which == 5 && choice[i] == "Blood C"){
+    console.log('zgadłeś')
+    i = i +1
+  } else if (which == 6 && choice[i] == "Blue Period"){
+    console.log('zgadłeś')
+    i = i +1
+  } else if (which == 7 && choice[i] == "Code geass"){
+    console.log('zgadłeś')
+    i = i +1
+  } else if (which == 8 && choice[i] == "Erased"){
+    console.log('zgadłeś')
+    i = i +1
+  } else if (which == 9 && choice[i] == "My hero academia"){
+    console.log('zgadłeś')
     i = i +1
   } else {
     console.log('dupa')
