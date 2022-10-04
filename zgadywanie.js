f278c45babfaf427e464cf3e7dfa730806461329
Sysek123 <szymekdzik123@gmail.com>
2022-10-04 22:00:59 +0200

    sound

--- zgadywanie.js
+++ zgadywanie.js
@@ -2,8 +2,11 @@ const videoPlayer = document.querySelector('.video-player')
 const video = videoPlayer.querySelector('.video')
 const playButton = videoPlayer.querySelector('.play-button')
 // TODO asd
+var allanswers = ["Full Metal Alchemist Brotherhood","Fire Force","Dororo", "Parasyte", "Baccano", "Blood C", "Blue Period", "Code geass", "Erased", "My hero academia"]
+var allso
 var answers = ["Full Metal Alchemist Brotherhood","Fire Force","Dororo", "Parasyte", "Baccano", "Blood C", "Blue Period", "Code geass", "Erased", "My hero academia"]
 var videoSources = ["files/FMAOP1.mp4","files/FF OP1.mp4", "files/Dororo OP1.mp4", "files/Parasyte OP1.mp4", "files/Baccano OP1.mp4", "files/Blood C OP1.mp4","files/Blue Period OP1.mp4", "files/Code geass OP1.mp4", "files/Erased OP1.mp4", "files/Hero OP1.mp4"]
+var sounds = ["files/fma sound.mp4","files/fire force sound.mp4", "files/dororo sound.mp4", "files/parasyte sound.mp4", "files/baccano sound.mp4", "files/blood c sound.mp4","files/blue period sound.mp4", "files/code geass sound.mp4", "files/erased sound.mp4", "files/my hero sound.mp4" ]
 let choice = []
 i = 0
 
@@ -17,13 +20,16 @@ playButton.addEventListener('click', (e) => {
     }
 })
 
-function next (){
-    var myVideo = document.getElementsByTagName('video')[0];
-    which = Math.round(Math.random() * ((videoSources.length - 1) - 0) + 0) 
-    myVideo.src = videoSources[which]
-    myVideo.load();
-    myVideo.play();
-}
+function playMusic(){
+  var myVideo = document.getElementsByTagName('video')[0];
+  myVideo.load();
+  var audio = document.getElementById('sound')
+  var music = document.getElementById('audioSource');
+  which = Math.round(Math.random() * ((sounds.length - 1) - 0) + 0) 
+  music.src = sounds[which]
+  audio.load()
+  audio.play();
+  }
 
 let sortedanswers = answers.sort();
 let input = document.getElementById("input");
@@ -60,12 +66,18 @@ function removeElements() {
 
 function check() {
   choice.push(value)
+  var audio = document.getElementById('sound')
+  audio.load()
   if (which == 0 && choice[i] == "Full Metal Alchemist Brotherhood" ) { 
     console.log('zgadłeś')
     var img = document.createElement("img")
     img.src = "files/okejka.png"
     var tag = document.getElementsByClassName("slot")[i]
     tag.appendChild(img)
+    var myVideo = document.getElementsByTagName('video')[0];
+    myVideo.src = videoSources[which]
+    myVideo.load();
+    myVideo.play();
     i = i +1
   } else if (which == 1 && choice[i] == "Fire Force"){
     console.log('zgadłeś')
@@ -73,6 +85,10 @@ function check() {
     img.src = "files/okejka.png"
     var tag = document.getElementsByClassName("slot")[i]
     tag.appendChild(img)
+    var myVideo = document.getElementsByTagName('video')[0];
+    myVideo.src = videoSources[which]
+    myVideo.load();
+    myVideo.play();
     i = i +1
   } else if (which == 2 && choice[i] == "Dororo"){
     console.log('zgadłeś')
@@ -80,6 +96,10 @@ function check() {
     img.src = "files/okejka.png"
     var tag = document.getElementsByClassName("slot")[i]
     tag.appendChild(img)
+    var myVideo = document.getElementsByTagName('video')[0];
+    myVideo.src = videoSources[which]
+    myVideo.load();
+    myVideo.play();
     i = i +1
   } else if (which == 3 && choice[i] == "Parasyte"){
     console.log('zgadłeś')
@@ -87,6 +107,10 @@ function check() {
     img.src = "files/okejka.png"
     var tag = document.getElementsByClassName("slot")[i]
     tag.appendChild(img)
+    var myVideo = document.getElementsByTagName('video')[0];
+    myVideo.src = videoSources[which]
+    myVideo.load();
+    myVideo.play();
     i = i +1
   } else if (which == 4 && choice[i] == "Baccano"){
     console.log('zgadłeś')
@@ -94,6 +118,10 @@ function check() {
     img.src = "files/okejka.png"
     var tag = document.getElementsByClassName("slot")[i]
     tag.appendChild(img)
+    var myVideo = document.getElementsByTagName('video')[0];
+    myVideo.src = videoSources[which]
+    myVideo.load();
+    myVideo.play();
     i = i +1
   } else if (which == 5 && choice[i] == "Blood C"){
     console.log('zgadłeś')
@@ -101,6 +129,10 @@ function check() {
     img.src = "files/okejka.png"
     var tag = document.getElementsByClassName("slot")[i]
     tag.appendChild(img)
+    var myVideo = document.getElementsByTagName('video')[0];
+    myVideo.src = videoSources[which]
+    myVideo.load();
+    myVideo.play();
     i = i +1
   } else if (which == 6 && choice[i] == "Blue Period"){
     console.log('zgadłeś')
@@ -108,6 +140,10 @@ function check() {
     img.src = "files/okejka.png"
     var tag = document.getElementsByClassName("slot")[i]
     tag.appendChild(img)
+    var myVideo = document.getElementsByTagName('video')[0];
+    myVideo.src = videoSources[which]
+    myVideo.load();
+    myVideo.play();
     i = i +1
   } else if (which == 7 && choice[i] == "Code geass"){
     console.log('zgadłeś')
@@ -115,6 +151,10 @@ function check() {
     img.src = "files/okejka.png"
     var tag = document.getElementsByClassName("slot")[i]
     tag.appendChild(img)
+    var myVideo = document.getElementsByTagName('video')[0];
+    myVideo.src = videoSources[which]
+    myVideo.load();
+    myVideo.play();
     i = i +1
   } else if (which == 8 && choice[i] == "Erased"){
     console.log('zgadłeś')
@@ -122,6 +162,10 @@ function check() {
     img.src = "files/okejka.png"
     var tag = document.getElementsByClassName("slot")[i]
     tag.appendChild(img)
+    var myVideo = document.getElementsByTagName('video')[0];
+    myVideo.src = videoSources[which]
+    myVideo.load();
+    myVideo.play();
     i = i +1
   } else if (which == 9 && choice[i] == "My hero academia"){
     console.log('zgadłeś')
@@ -129,6 +173,10 @@ function check() {
     img.src = "files/okejka.png"
     var tag = document.getElementsByClassName("slot")[i]
     tag.appendChild(img)
+    var myVideo = document.getElementsByTagName('video')[0];
+    myVideo.src = videoSources[which]
+    myVideo.load();
+    myVideo.play();
     i = i +1
   } else {
     console.log('dupa')
